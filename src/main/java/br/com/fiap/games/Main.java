34813c7ba4d8c8a45958e4c403beedbbc1211b99
Guilherme Romera <g.romera2018@icloud.com>
2026-05-05 19:54:41 -0300

    JPA lifecycle

--- src/main/java/br/com/fiap/games/Main.java
+++ src/main/java/br/com/fiap/games/Main.java
@@ -14,13 +14,29 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        Game game1 = new Game();
+        game1.setTitulo("Ikari Warriors");
+        game1.setCategoria("Arcade");
+        game1.setDataLancamento(LocalDate.of(1986, 1, 1));
+        game1.setFinalizado(true);
+        game1.setProdutora("SNK");
+        game1.setValor(256.88);
+
         EntityManager em = Conexao.getEntityManager();
+        GameDao gameDao = new GameDao(em);
+
+        em.getTransaction().begin();
+        gameDao.salvar(game1);
+        game1.setTitulo("Ikari Warriors SNK");
+        em.getTransaction().commit();
+        em.close();
 
         //pesquisar(em);
         //cadastrar(em);
         //listarTodosOsGames(em);
-        buscarGamesPorFaixaDeValores(em);
-        em.close();
+        //buscarGamesPorFaixaDeValores(em);
+        //em.close();
 
     }
 
